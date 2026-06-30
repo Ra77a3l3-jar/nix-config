669e2859921fdf4574f2ad9843eb3601d4cd327d
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2026-06-30 11:23:39 +0200

    Add oh-my-posh prompt and zsh config

--- modules/shell/fish.nix
+++ modules/shell/fish.nix
@@ -1,59 +1,22 @@
-{ pkgs, ... }:
+{ pkgs, pkgs-unstable, ... }:
 
 {
   programs.fish = {
     enable = true;
-    
+
     interactiveShellInit = ''
       set -g fish_greeting ""
-      
+
       fish_add_path $HOME/.local/bin
       fish_add_path $HOME/.nix-profile/bin
-      fish_add_path /usr/local/cuda-13.1/bin
-      set -x LD_LIBRARY_PATH /usr/local/cuda-13.1/lib64 $LD_LIBRARY_PATH
       
       if type -q zoxide
         zoxide init fish | source
       end
-      
-      if type -q zellij
-        if not set -q ZELLIJ
-          exec zellij
-        end
+
+      if type -q oh-my-posh
+        oh-my-posh init fish --config ${pkgs-unstable.oh-my-posh}/share/oh-my-posh/themes/robbyrussell.omp.json | source
       end
     '';
-    
-    functions = {
-      fish_prompt = ''
-        set_color E36464
-        echo -n '@'(whoami)' '
-        
-        set_color 62ED8B
-        echo -n '➜ '
-        
-        set_color 56B6C2
-        echo -n (basename (prompt_pwd))
-        
-        if type -q git
-          set -l branch (git rev-parse --abbrev-ref HEAD 2>/dev/null)
-          if test -n "$branch"
-            set_color DDB15F
-            echo -n ' git('
-            set_color D4AAFC
-            echo -n "$branch"
-            set_color DDB15F
-            echo -n ')'
-          end
-        end
-        
-        if test $status -ne 0
-          set_color DCB977
-          echo -n ' ✗'
-        end
-        
-        set_color normal
-        echo -n ' '
-      '';
-    };
   };
 }
