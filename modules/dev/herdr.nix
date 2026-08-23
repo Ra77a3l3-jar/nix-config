19f5a5422620309a53bf24d079a836625be66d31
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2026-08-23 15:53:46 +0200

    Update herdr config and browser and code plugin

--- modules/dev/herdr.nix
+++ modules/dev/herdr.nix
@@ -1,13 +1,21 @@
-{ herdr, pkgs, ... }:
+{
+  inputs,
+  herdrPlugins,
+  ...
+}:
 
 {
   imports = [
-    herdr.homeManagerModules.default
+    inputs.herdnix.homeManagerModules.default
   ];
 
   programs.herdr = {
     enable = true;
-    package = herdr.packages.${pkgs.system}.default;
+
+    plugins = with herdrPlugins; [
+      terminalBrowser
+      terminalCode
+    ];
 
     settings = {
 
@@ -23,6 +31,8 @@
         pane_borders = true;
         pane_gaps = false;
       };
+
+      experimental.kitty_graphics = true;
 
       keys = {
         prefix = "ctrl+v";
@@ -43,9 +53,59 @@
 
         command = [
           {
-            key = "prefix+f";
-            type = "shell";
-            command = "herdr plugin action invoke open-file-viewer-tab --plugin herdr-file-viewer";
+            key = "prefix+shift+1";
+            type = "plugin_action";
+            command = "herdr-file-viewer.open-file-viewer-tab";
+          }
+          {
+            key = "prefix+shift+2";
+            type = "plugin_action";
+            command = "cloudmanic.herdr-plus.projects";
+          }
+          {
+            key = "prefix+shift+3";
+            type = "plugin_action";
+            command = "cloudmanic.herdr-plus.quick-actions";
+          }
+          {
+            key = "prefix+shift+4";
+            type = "plugin_action";
+            command = "herdr-spreader.apply";
+          }
+          {
+            key = "prefix+shift+5";
+            type = "plugin_action";
+            command = "persiyanov.reviewr.toggle";
+          }
+          {
+            key = "prefix+shift+6";
+            type = "plugin_action";
+            command = "official.browser.open-localhost";
+          }
+          {
+            key = "prefix+shift+7";
+            type = "plugin_action";
+            command = "mirror.start";
+          }
+          {
+            key = "prefix+shift+8";
+            type = "plugin_action";
+            command = "herdr-sidebar.open-sidebar";
+          }
+          {
+            key = "prefix+shift+9";
+            type = "plugin_action";
+            command = "nicosuave.memex.desk";
+          }
+          {
+            key = "prefix+shift+0";
+            type = "plugin_action";
+            command = "zenbu-labs.terminal-browser.open-split";
+          }
+          {
+            key = "prefix+shift+minus";
+            type = "plugin_action";
+            command = "zenbu-labs.tode.open-split";
           }
         ];
       };
